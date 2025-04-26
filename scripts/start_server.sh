edd4fa62c786ee0442cda1b33cc1ef51be73314f
Kazaraal <93543259+Kazaraal@users.noreply.github.com>
2025-04-26 13:47:40 +0100

    Cleaned up start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo systemctl start tomcat.service
-sudo systemctl enable tomcat.service
-sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
+sudo systemctl enable --now tomcat.service
+sudo systemctl enable --now httpd.service
 
