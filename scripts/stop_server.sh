00b686533fb202440ee41cf4fdd61b2f341351e7
Kazaraal <93543259+Kazaraal@users.noreply.github.com>
2025-04-27 09:17:39 +0100

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Stop Apache if running
-if systemctl is-active --quiet httpd; then
-  sudo systemctl stop httpd
-fi
-
-# Stop Tomcat if running
-if systemctl is-active --quiet tomcat; then
-  sudo systemctl stop tomcat
-fi
+# Stop Tomcat and Apache
+sudo systemctl stop tomcat.service
+sudo systemctl stop httpd.service
